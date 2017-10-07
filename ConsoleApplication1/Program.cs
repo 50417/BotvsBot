ecb2c2aca41e57c57ba2f2da64aef5f1ea7ef453
Sohil <sohil77@live.com>
2017-10-07 12:20:44 -0700

    First Commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Test = new Class1();
+            //Test.foo();
+            MachineLearningNaive hello = new MachineLearningNaive();
+            hello.runNaive();
+            TestProgram program = new TestProgram();
+            String yes = program.foo(122);
+        }
+    }
+}
---
+++ ConsoleApplication1/TestProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class TestProgram
+    {
+        public String foo(int p)
+        {
+            if (p > 0)
+            {
+                return "a" + p;
+            }
+            else
+                return "b" + (-p);
+        }
+    }
+}
